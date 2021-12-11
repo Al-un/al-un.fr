4c4364fe64014301b2ad2325a204b6fde269bc23
Al-un <alun.sng@gmail.com>
2021-12-11 18:29:42 +0900

    apps: bootstrap a PoC

---
+++ packages/apps/src/components/Pouet.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="pouet">
+    <super-button @click="onClick">{{ msg }}</super-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Pouet",
+  props: {
+    msg: String,
+  },
+  setup(){
+    return {
+      onClick(){
+        console.log("CLICK! CLICK!")
+      }
+    }
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
