45376a957d053005625c832756945859dc517749
Al-un <github@al-un.fr>
2025-01-01 18:21:25 +0900

    update main page name

--- apps/src/apps/2048/index.ts
+++ apps/src/apps/2048/index.ts
@@ -1,9 +1,9 @@
 import type { AlApp } from '@/types'
 
-import HomeView from './views/HomeView.vue'
+import GamePage from './views/GamePage.vue'
 
 export const game2048App: AlApp = {
   name: '2048',
   routeRoot: '2048',
-  routes: [{ path: '', component: HomeView }]
+  routes: [{ path: '', component: GamePage }]
 }
